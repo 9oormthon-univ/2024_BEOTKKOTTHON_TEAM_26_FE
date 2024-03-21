959863f7f8c55940d7fdb06bce17955b115ee154
yoonseopkim <qc70045@gmail.com>
2024-03-21 23:31:45 +0900

    [CHORE] index.js 라우팅 수정

--- goorm/src/App.js
+++ goorm/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainFunding from "./pages/main/MainFunding";
 import Contest from "./pages/main/Contest";
+import Categories from "./pages/Categories";
+import Detail from "./pages/Detail";
+import Test from "./pages/Test";
 
 function App() {
   return (
